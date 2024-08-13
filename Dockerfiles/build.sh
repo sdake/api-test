9e671bc4dd2c16c4ae5a6341848157e8650aa017
Steven Dake <steve@computelify.com>
2024-08-13 21:44:47 +0000

    Reorganize repo into directories by function

---
+++ Dockerfiles/build.sh
@@ -0,0 +1,7 @@
+API_VERSION=0.1.1
+date=$(date '+%Y%m%d%H%M%S')
+
+docker buildx build --tag docker.io/sdake/test-api:v${API_VERSION} --output type=docker --progress plain .
+docker buildx build --tag docker.io/sdake/test-api:v${API_VERSION} --output type=docker --progress plain .
+
+docker push docker.io/sdake/test-api:${API_VERSION}
