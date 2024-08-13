12430a89e37b273dca80383d9b04af6b2703d20d
Steven Dake <steve@computelify.com>
2024-08-13 21:46:55 +0000

    Manifest

--- Dockerfiles/build.sh
+++ Dockerfiles/build.sh
@@ -4,4 +4,4 @@ date=$(date '+%Y%m%d%H%M%S')
 docker buildx build --tag docker.io/sdake/test-api:v${API_VERSION} --output type=docker --progress plain .
 docker buildx build --tag docker.io/sdake/test-api:v${API_VERSION} --output type=docker --progress plain .
 
-docker push docker.io/sdake/test-api:${API_VERSION}
+docker push docker.io/sdake/test-api:v${API_VERSION}
